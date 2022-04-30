9aab54a6e31ef0c9e2e5e467bd40381f02897eb6
Snowdevil - Highfly Chastot <mjetzer@live.com>
2022-04-30 10:57:53 -0500

    Still reworking PATH system to work with creating a database in db folder

--- py/calculations/fileImport.py
+++ py/calculations/fileImport.py
@@ -3,30 +3,31 @@ import os
 import os.path
 from config.definitions import ROOT_DIR
 
-def saveMachine (machine, time):
+def saveMachine ():
     
-    print(os.path.join(ROOT_DIR, 'py', 'testing', 'test.py'))
-
-    #Gets current working directory
-    currentPath = os.getcwd()
+    databasePath = os.path.join(ROOT_DIR, 'db')
+    print(databasePath)
     
     #Changes current directory to db directory and sets to active
-    os.chdir('..')
+    os.chdir(databasePath)
 
-    #Creates database if not created, otherwise connects to it
-    setupDb = sql.connect('machineDatabase.db')
-    cursor = setupDb.cursor()
+    cwd = os.getcwd()
+    print(cwd)
+
+    # #Creates database if not created, otherwise connects to it
+    # setupDb = sql.connect('machineDatabase.db')
+    # cursor = setupDb.cursor()
     
-    #sql statement
-    sql1 = ''' INSERT INTO Machines(machine_name,completion_time)
-            VALUES(?,?)'''
-    sql2 = '''CREATE TABLE IF NOT EXISTS Machines
-                  (Machine_Name TEXT, Completion_Time TEXT)'''
+    # #sql statement
+    # sql1 = ''' INSERT INTO Machines(machine_name,completion_time)
+    #         VALUES(?,?)'''
+    # sql2 = '''CREATE TABLE IF NOT EXISTS Machines
+    #               (Machine_Name TEXT, Completion_Time TEXT)'''
 
-    #Creates table if not created, otherwise will return machine name and time left
-    cursor.execute(sql2)
-    cursor.execute(sql1)             
-    setupDb.commit
-    setupDb.close
+    # #Creates table if not created, otherwise will return machine name and time left
+    # cursor.execute(sql2)
+    # cursor.execute(sql1)             
+    # setupDb.commit
+    # setupDb.close
 
-saveMachine("Tsugami 5", "asd")
+saveMachine()
