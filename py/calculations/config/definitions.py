679eee688afd50f021755a3ab3ccab737357b507
Snowdevil - Highfly Chastot <mjetzer@live.com>
2022-04-30 21:35:14 -0500

    Fixed PATH stuff, now working on sqlite3 implementation

--- py/calculations/config/definitions.py
+++ py/calculations/config/definitions.py
@@ -1,3 +1,8 @@
 import os
+from re import I
 
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+
+def climbDirectory (height):
+    for i in range(height):
+        os.chdir('..')
--- py/calculations/fileImport.py
+++ py/calculations/fileImport.py
@@ -1,33 +1,32 @@
 import sqlite3 as sql
 import os
 import os.path
-from config.definitions import ROOT_DIR
+from config.definitions import (
+    ROOT_DIR,
+    climbDirectory
+)
 
 def saveMachine ():
     
-    databasePath = os.path.join(ROOT_DIR, 'db')
-    print(databasePath)
+    #Find current path of this folder
+    currentPath = os.path.join(ROOT_DIR)
     
-    #Changes current directory to db directory and sets to active
-    os.chdir(databasePath)
+    #Changes current directory to db directory in relation to this file
+    os.chdir(currentPath)
+    climbDirectory(2)
+    os.chdir('db')
 
-    cwd = os.getcwd()
-    print(cwd)
-
-    # #Creates database if not created, otherwise connects to it
-    # setupDb = sql.connect('machineDatabase.db')
-    # cursor = setupDb.cursor()
+    #Creates database if not created, otherwise connects to it
+    setupDb = sql.connect('machineDatabase.db')
+    cursor = setupDb.cursor()
     
-    # #sql statement
-    # sql1 = ''' INSERT INTO Machines(machine_name,completion_time)
-    #         VALUES(?,?)'''
-    # sql2 = '''CREATE TABLE IF NOT EXISTS Machines
-    #               (Machine_Name TEXT, Completion_Time TEXT)'''
+    #sql statement
+    sql2 = '''CREATE TABLE IF NOT EXISTS Machines
+                  (Machine_Name TEXT, Completion_Time TEXT)'''
 
-    # #Creates table if not created, otherwise will return machine name and time left
-    # cursor.execute(sql2)
-    # cursor.execute(sql1)             
-    # setupDb.commit
-    # setupDb.close
+    #Creates table if not created, otherwise will return machine name and time left
+    cursor.execute(sql2)            
+    setupDb.commit
+    setupDb.close
 
 saveMachine()
