f7e0c26f43fc6511726794ff9fcf5150c89dabfb
Mike <mjetzer@live.com>
2022-05-01 20:58:51 -0500

    Database working for reading and writing variables and parameters

--- py/calculations/fileImport.py
+++ py/calculations/fileImport.py
@@ -21,20 +21,43 @@ def saveMachine (Machine_Name, Completion_Time):
     cursor = setupDb.cursor()
 
     #Creates table if not created, otherwise will return machine name and time left
+    cursor.execute('''DELETE FROM Machines WHERE Machine_Name = '%s' ''' % Machine_Name)
+       
     cursor.execute('''CREATE TABLE IF NOT EXISTS Machines
-    (Machine_Name TEXT, Completion_Time TEXT)''')
+    (Machine_Name TEXT, Completion_Time INT)''')
     
     cursor.execute('''INSERT INTO Machines (Machine_Name, Completion_Time) 
     VALUES (?, ?)''', (Machine_Name, Completion_Time))
      
-    setupDb.commit
+    setupDb.commit()
     
     cursor.execute("SELECT * FROM Machines")
+    print(cursor.fetchall())
+    
+    setupDb.close
+
+#saveMachine("Tsugami 5", "3/15/2022")
+
+def readMachine (Machine_Name):
+    
+    #Find current path of this folder
+    currentPath = os.path.join(ROOT_DIR)
+    
+    #Changes current directory to db directory in relation to this file
+    os.chdir(currentPath)
+    climbDirectory(2)
+    os.chdir('db')
+
+    #Creates database if not created, otherwise connects to it
+    setupDb = sql.connect('machineDatabase.db')
+    cursor = setupDb.cursor()
+    
+    cursor.execute('''SELECT * FROM Machines WHERE Machine_Name = '%s' ''' % Machine_Name)
     rows = cursor.fetchall()
     
     for row in rows:
     	print(rows)
     
     setupDb.close
-
-saveMachine("Tsugami 5", "3/15/2022")
+    
+#readMachine("Tsugami 5")
