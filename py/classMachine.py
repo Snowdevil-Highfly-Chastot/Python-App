edb8b829c39b421498697a52507f126bb8a35d23
Michael <mjetzer@live.com>
2022-05-05 13:23:06 -0500

    Added barfeed functionality to Class, and refactored.

--- py/classMachine.py
+++ py/classMachine.py
@@ -1,6 +1,8 @@
-from machineCalculations import (
+import datetime
+from machineClassLibrary import (
 runTimeLeft,
-completionTime
+completionTime,
+barfeedParts
 )
 
 from fileImport import (
@@ -9,10 +11,15 @@ readMachine
 )
 
 class machine:
-    def __init__(self, name, partsNeeded, partTime):
+    def __init__(self, name, partsNeeded = 0, partTime = 0, partLength = 0, barLength = 0, cutoffWidth = 0, barfeedParameter = 0):
+        
         self.name = name
         self.partTime = partTime
         self.partsNeeded = partsNeeded
+        self.partLength = partLength
+        self.barLength = barLength
+        self.cutoffWidth = cutoffWidth
+        self.barfeedParameter = barfeedParameter
     
     def jobFinished(self):
         
@@ -23,30 +30,58 @@ class machine:
     	
     	print("posting...")
     	saveMachine(self.name, completionTime(self.partTime, self.partsNeeded))
-    	
 
     def pullCompletionTime(self):
 
         print("fetching...")
         readMachine(self.name)
         
+    def barfeedParts(self):
         
-machine1 = machine("Tsugami 5", 117, 117)
+        totalParts = barfeedParts(self.partLength, self.cutoffWidth, self.barfeedParameter, self.barLength)
+        
+        print(totalParts, "Parts Per Bar")
+        return totalParts
+        
+    def barfeedCompletionTime(self):
+    
+        totalParts = barfeedParts(self.partLength, self.cutoffWidth, self.barfeedParameter, self.barLength)
+    
+#Set variables and calculate timing
+        totalTime = self.partTime * totalParts
+        now = datetime.datetime.now()
+        timeCompleted = now + datetime.timedelta(seconds = totalTime)
+        
+        print(timeCompleted, "End of bar")
+        return timeCompleted
+        
+    def barfeedTime(self):
+    
+        totalParts = barfeedParts(self.partLength, self.cutoffWidth, self.barfeedParameter, self.barLength)
+        runTimeLeft(totalParts, self.partTime)
+ 
+       
+#Initializing Class below for testing
+        
+machine1 = machine("Tsugami 5", 1200, 117, 2.625, 144, 0.95, 3.5)
 #machine1.postCompletionTime()
+machine1.barfeedParts()
+machine1.barfeedCompletionTime()
+machine1.barfeedTime()
 machine1.pullCompletionTime()
 print(" ")
 
-machine2 = machine("Tsugami 6", 109, 109)
+machine2 = machine("Tsugami 6", 4000, 109)
 #machine2.postCompletionTime()
 machine2.pullCompletionTime()
 print(" ")
 
-machine3 = machine("Tsugami 7", 59, 59)
+machine3 = machine("Tsugami 7", 1000, 59)
 #machine3.postCompletionTime()
 machine3.pullCompletionTime()
 print(" ")
 
-machine4 = machine("Tsugami 8", 42, 42)
+machine4 = machine("Tsugami 8", 6000, 42)
 #machine4.postCompletionTime()
 machine4.pullCompletionTime()
 print(" ")
