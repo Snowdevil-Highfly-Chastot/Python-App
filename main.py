742582e0d4f2e640887e9100df8ca689a87d5de5
Snowdevil - Highfly Chastot <mjetzer@live.com>
2022-05-05 19:08:14 -0500

    Messing around with Kivy

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-import kivy
-kivy.require('2.0.0') # replace with your current kivy version !
-
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.graphics import Color, Rectangle
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+
+
+class RootWidget(FloatLayout):
+
+    def __init__(self, **kwargs):
+        # make sure we aren't overriding any important functionality
+        super(RootWidget, self).__init__(**kwargs)
+
+        # let's add a Widget to this layout
+        self.add_widget(
+            Button(
+                text="Hello World",
+                size_hint=(.80, .90),
+                pos_hint={'center_x': .5, 'center_y': .5}))
 
 
 class MainApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        self.root = root = RootWidget()
+        root.bind(size=self._update_rect, pos=self._update_rect)
 
+        with root.canvas.before:
+            Color(0, 1, 1, .6)  # colors range from 0-1 not 0-255
+            self.rect = Rectangle(size=root.size, pos=root.pos)
+        return root
+
+    def _update_rect(self, instance, value):
+        self.rect.pos = instance.pos
+        self.rect.size = instance.size
 
 if __name__ == '__main__':
     MainApp().run()
