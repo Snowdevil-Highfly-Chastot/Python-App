67ff1b6c023162fd414f247dfa6e398b241a0e50
Michael <mjetzer@live.com>
2022-05-10 19:10:08 -0500

    Pushing any Kivy changes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.colorpicker import ColorPicker
 from py.fileImport import readMachine
 from kivy.lang import Builder
 from kivy.uix.anchorlayout import AnchorLayout
